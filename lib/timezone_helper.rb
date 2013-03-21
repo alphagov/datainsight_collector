657d41f18efc47481e8c0124bfb1c20bcec72e9e
Max Fliri <mfliri@thoughtworks.com>
2013-03-21 16:19:05 +0000

    DateTime extension method renamed and accepts tz as argument
    @maxfliri
    @gtrogers

--- lib/timezone_helper.rb
+++ lib/timezone_helper.rb
@@ -1,8 +1,8 @@
 require "tzinfo"
 
 class DateTime
-  def to_local_timezone
-    offset = TZInfo::Timezone.get("Europe/London").period_for_utc(self).utc_total_offset_rational
+  def with_tz_offset(region)
+    offset = TZInfo::Timezone.get(region).period_for_utc(self).utc_total_offset_rational
     (self - offset).new_offset(offset)
   end
 end
